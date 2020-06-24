cf125ab7c4298f6d1901d182a354909b9259642b
Leedabord <gwforumlakers@gmail.com>
2020-06-23 23:39:12 -0700

    2 files modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,8 @@ const routes: Routes = [
   { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
   { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
   { path: 'register', loadChildren: './register/register.module#RegisterPageModule' },
+  { path: 'home', loadChildren: 
+  './home/home.module#HomePageModule' },
   { path: 'details/:id', loadChildren: './details/details.module#DetailsPageModule' },
   { path: 'new-task', loadChildren: './new-task/new-task.module#NewTaskPageModule' }
 ];
