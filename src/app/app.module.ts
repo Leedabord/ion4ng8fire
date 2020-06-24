ddaa2863f6e9e2701ba90023b369dddf4dce9f87
Leedabord <gwforumlakers@gmail.com>
2020-06-23 22:48:25 -0700

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,21 +9,29 @@ import { AppComponent } from './app.component';
 
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';  
+// , FirestoreSettingsToken
+import { AngularFireStorageModule } from '@angular/fire/storage';
+
 import { environment } from '../environments/environment';
 import { FirebaseAuthService } from './firebase-auth.service';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Tab2aPage } from './tab2a/tab2a.page';
+// import { NewTaskModalPage } from './new-task-modal/new-task-modal.page';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    Tab2aPage
-  ],
-  entryComponents: [
-    Tab2aPage
-  ],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,  AngularFireModule.initializeApp(environment.firebase),
-    AngularFireAuthModule
+  declarations: [ AppComponent, Tab2aPage ],
+  entryComponents: [ Tab2aPage ],
+  // declarations: [AppComponent, NewTaskModalPage],
+  // entryComponents: [NewTaskModalPage],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule, // firestore
+    AngularFireStorageModule, // storage
+    FormsModule,
+    ReactiveFormsModule
 ],
   providers: [
     FirebaseAuthService,
