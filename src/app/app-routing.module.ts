772d7dc767b4b2fc77561b6160ae6053b40b77ce
Leedabord <gwforumlakers@gmail.com>
2020-06-23 22:08:31 -0700

    1 file modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,26 +3,27 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' }
   // { path: '', redirectTo: 'login', pathMatch: 'full' },
-  // { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
-/*   { path: 'register', loadChildren: './register/register.module#RegisterPageModule' },
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
+  { path: 'register', loadChildren: './register/register.module#RegisterPageModule' },
   { path: 'details/:id', loadChildren: './details/details.module#DetailsPageModule' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'new-task', loadChildren: './new-task/new-task.module#NewTaskPageModule' }
+];
+
+/* 
   // { path: 'new-task-modal', loadChildren: './new-task-modal/new-task-modal.module#NewTaskModalPageModule' }
 
- // Regular Route 
+  // Regular Route 
   { path: 'eager', component: MyComponent },
 
   // Lazy Loaded Route (Page)
   { path: 'lazy', loadChildren: './lazy/lazy.module#LazyPageModule' },
 
-    // Redirect
+  // Redirect
   { path: 'here', redirectTo: 'there', pathMatch: 'full' }
-
 */
-];
 
 @NgModule({
   imports: [
